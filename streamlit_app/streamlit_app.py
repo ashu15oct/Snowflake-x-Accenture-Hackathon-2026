97f1ed7e0c6f18a8d11f11422ebfd022aae43e40
Ashutosh Mishra <ashutosh.br.mishra@accenture.com>
2026-01-08 09:09:06 +0000

    add agent selection option

--- streamlit_app/streamlit_app.py
+++ streamlit_app/streamlit_app.py
@@ -1,27 +1,46 @@
 import streamlit as st
 import _snowflake
 import json
-st.title("List Cortex Agents")
 
 DB = "RETAIL_DB"
 SCHEMA = "ABT_BUY"
+
 TIMEOUT_MS = 30000
 
-# Call the API
-path = f"/api/v2/databases/{DB}/schemas/{SCHEMA}/agents"
-resp = _snowflake.send_snow_api_request("GET", path, {}, {}, None, None, TIMEOUT_MS)
+def get_agent_data():
+    """
+    Get Agent Data
+    """
+    path = f"/api/v2/databases/{DB}/schemas/{SCHEMA}/agents"
+    resp = _snowflake.send_snow_api_request("GET", path, {}, {}, None, None, TIMEOUT_MS)
+    
+    # Parse JSON
+    data = json.loads(resp["content"])
+    return data.get("data", [])
 
-# Parse JSON
-data = json.loads(resp["content"])
-agents = data.get("data", [])
+def select_agent(agents):
+    """
+    Select Agent
+    """
+    st.header("Agent List")
+    st.subheader("Select Agent")
+    
+    labels = [agent['profile']['display_name'] for agent in agents]
+    selected_display = st.radio("", labels, label_visibility = "collapsed")
+    selected_agent = next(agent for agent in agents if agent['profile']['display_name'] == selected_display)
+    return selected_agent
+    
+def main():
+    st.title("Retail Intelligence Platform")
 
-# Show each agent vertically
-for agent in agents:
-    st.markdown("---")
-    st.write(f"**Display Name:** {agent.get('profile', {}).get('display_name')}")
-    st.write(f"**Object Name:** {agent.get('name')}")
-    st.write(f"**Comment:** {agent.get('comment')}")
-    st.write(f"**Database:** {agent.get('database_name')}")
-    st.write(f"**Schema:** {agent.get('schema_name')}")
-    st.write(f"**Owner:** {agent.get('owner')}")
-    st.write(f"**Created On:** {agent.get('created_on')}")
+    agents = get_agent_data()
+    agent_object = select_agent(agents)
+    agent_object_name = agent_object['name']
+    
+    api = f"/api/v2/databases/{DB}/schemas/{SCHEMA}/agents/{agent_object_name}:run"
+
+    st.subheader(f"Ask the {agent_object['profile']['display_name']}")
+    prompt = st.text_input("", "Show price comparisons", label_visibility="collapsed")
+
+if __name__ == "__main__":
+    main()
